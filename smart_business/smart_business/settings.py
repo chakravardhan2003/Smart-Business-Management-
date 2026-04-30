39d4527cd15d0480bc2c0a5dd74d141303d5abe4
Sirisha <p.v.n.s.sirisha2004@gmail.com>
2026-04-30 19:18:01 +0530

    my changes

--- smart_business/smart_business/settings.py
+++ smart_business/smart_business/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'smart_business.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        ''DIRS': [BASE_DIR / 'smart_business' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
